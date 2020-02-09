2637173cf43429c740681a1d42c5be3c6920ecd2
Treesorn <treesorn@Treesorns-MacBook-Pro.local>
2020-02-09 23:55:44 +1100

    map

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPage implements OnInit {
 
+  @ViewChildren("map") mapElement;
+  map: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.initMap();
+  }
+
+  initMap(){
+    let coords = new google.maps.LatLng(-35.473469, 149.012375);
+    let mapOptions: google.maps.MapOptions = {
+      center: coords,
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement,
+      mapOptions)
+
+      let marker: google.maps.Marker = new google.maps.Marker({
+        map: this.map,
+        position: coords
+      })
   }
 
 }
