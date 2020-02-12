a7b8f4f70dfe2cdddf775e735c90eaa33b51c20b
Treesorn <treesorn@Treesorns-MacBook-Pro.local>
2020-02-13 01:33:32 +1100

    minor fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFirestore} from '@angular/fire/firestore';
 
 import { environment } from '../environments/environment';
 // modal imports
@@ -46,7 +45,6 @@ import { CalendarPageModule } from './calendar/calendar.module';
     AppRoutingModule,
     AngularFireModule.initializeApp( environment.firebase ),
     AngularFireAuthModule,
-    AngularFirestore,
     AngularFirestoreModule,
     AngularFireStorageModule,
     SignupPageModule,
